0b511aecf09f575249081bf7fb26b05a47c89aa4
Diganta Saha <diganta15-4898@diu.edu.bd>
2023-02-12 22:41:16 +0600

    Linked List advanced Creation

---
+++ Linked_list/linked_list_main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+
+{
+public:
+    int value;
+    Node *Next;
+
+    // constructor creation
+    Node(int val)
+    {
+        value = val;
+        Next = NULL;
+    }
+};
+
+
+
+void insertAtTail(Node* &head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL){
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    while(temp->Next != NULL)
+    {
+        temp = temp->Next;
+
+    }
+     temp->Next = newNode;
+}
+
+
+
+void display(Node *n)
+{
+    while (n != NULL)
+    {
+        cout << n->value;
+        if (n->Next != NULL)
+        {
+            cout << " -> ";
+        }
+        n = n->Next;
+    }
+    cout << endl
+         << endl;
+}
+
+int main()
+{
+Node *head = NULL;
+
+insertAtTail(head, 1);
+insertAtTail(head, 5);
+insertAtTail(head, 8);
+insertAtTail(head, 9);
+    display(head);
+
+}
