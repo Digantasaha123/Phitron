f04fa60fd79ab3937c565b6baf77f30de414851b
Diganta123 <diganta15-4898@diu.edu.bd>
2024-03-29 13:54:43 +0600

    um

--- Batch 4/Algorithm/Dynamic Programming/subset_sum_top_down.cpp
+++ Batch 4/Algorithm/Dynamic Programming/subset_sum_top_down.cpp
@@ -1,16 +1,11 @@
-// Some code
 #include <bits/stdc++.h>
 using namespace std;
 int dp[1005][1005];
 bool subset_sum(int n, int a[], int s)
 {
+
     if (n == 0)
-    {
-        if (s == 0)
-            return true;
-        else
-            return false;
-    }
+        return !s;
     if (dp[n][s] != -1)
         return dp[n][s];
     if (a[n - 1] <= s)
@@ -20,9 +15,7 @@ bool subset_sum(int n, int a[], int s)
         return dp[n][s] = op1 || op2;
     }
     else
-    {
         return dp[n][s] = subset_sum(n - 1, a, s);
-    }
 }
 int main()
 {
@@ -30,25 +23,14 @@ int main()
     cin >> n;
     int a[n];
     for (int i = 0; i < n; i++)
-    {
         cin >> a[i];
-    }
     int s;
     cin >> s;
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= s; j++)
-        {
-            dp[i][j] = -1;
-        }
-    }
-    if (subset_sum(n, a, s))
-    {
+    memset(dp, -1, sizeof(dp));
+    subset_sum(n, a, s);
+    if (dp[n][s])
         cout << "YES" << endl;
-    }
     else
-    {
         cout << "NO" << endl;
-    }
     return 0;
 }
