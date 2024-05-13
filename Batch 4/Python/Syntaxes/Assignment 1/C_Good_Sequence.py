c7619ac718901649079bd2a585461af4392e5d5e
Diganta123 <diganta15-4898@diu.edu.bd>
2024-05-13 20:21:25 +0600

    assignment 1 90% python

--- Batch 4/Python/Syntaxes/Assignment 1/C_Good_Sequence.py
+++ Batch 4/Python/Syntaxes/Assignment 1/C_Good_Sequence.py
@@ -1,10 +1,17 @@
-x = int(input())
-a = input()
-ar = list(map(int,a.split()))
-print(ar)
-dict = {}
-for i in range(x):
-    dict[ar[i]] = 0
-    
-for i in range(x):
-    if
+n = int(input())
+a = list(map(int, input().split()))
+dic = {}
+for num in a:
+    if num  in dic:
+        dic[num] += 1
+    else :
+        dic[num] =1
+ans = 0
+
+for num,cnt in dic.items():
+    if cnt > num :
+        ans += cnt-num
+    elif  num > cnt :
+        ans+=cnt
+print(ans)
+for i in num:
